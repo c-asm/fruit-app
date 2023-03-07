0e838b1641991931bd32d91e3eb1afaaeea187c4
c-asm <c-asm-coder@proton.me>
2023-03-07 22:54:06 +0300

    Small fix - url api port

--- src/config.js
+++ src/config.js
@@ -1,5 +1,8 @@
+const PORT = process.env.PORT || 3000;
+console.debug('Config: Port being used:', PORT);
+
 const config = {
-	baseApiUrl: 'http://localhost:80', // Dont do this in production!
+	baseApiUrl: `http://localhost:${PORT}`, // Dont do this in production!
 }
 
 export default config;
